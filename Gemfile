2b8a5af74cc4ecbc4215a80917e5b2dcf645a913
Kevin Emancipator <kevine@wharton.upenn.edu>
2014-01-10 16:23:22 -0500

    Updated

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 #ruby-gemset=railstutorial_rails_4_0
 ruby '1.9.3'
-gem 'rails'
+gem 'rails', "4.0.0"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -11,18 +11,24 @@ gem 'rails'
 
 group :development do
   gem 'sqlite3'
+  gem 'rspec-rails'
 end
 
 gem 'heroku'
-gem 'sass-rails',   '4.0.1'
-gem 'uglifier', '2.1.1'
-gem 'coffee-rails', '4.0.1'
-gem 'jquery-rails', '3.0.4'
-gem 'turbolinks', '1.1.1'
+
+group :test do
+	gem 'selenium-webdriver'
+	gem 'capybara'
+end
+
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
 gem 'bootstrap-sass'
 gem 'formtastic-bootstrap'
-gem 'jbuilder', '1.0.2'
-gem 'rspec-rails'
 
 group :doc do
 	gem 'sdoc', '0.3.20', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -20,9 +22,6 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-
-  config.include Capybara::DSL
-  #added according to Listing 3.10 in Hartl's guide
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -42,4 +41,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include Capybara::DSL
+  #added according to Listing 3.10 in Hartl's guide
 end
