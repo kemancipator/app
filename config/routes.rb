61b2be9aaa35d35c98f42a424c43071141dd5c2d
Kevin Emancipator <kevine@wharton.upenn.edu>
2014-01-26 23:24:06 -0500

    Statics Pages Pt. II

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ App::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contactus"
   resources :microposts
 
   resources :posts
